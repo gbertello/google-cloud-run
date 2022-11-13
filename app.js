694f40807d2f22bdefb2bd866f930cf9eedbb4d5
Guillaume Bertello <guillaume.bertello@gmail.com>
2022-11-13 19:34:29 +0100

    Refactor application to minimal coding

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 8080;
 
 app.get("/", function (req, res) {
-  res.send("Hello world!");
+  res.send("Hello world again!");
 });
 
-app.listen(port, function () {
-  console.log(`Google cloud run app listening on port ${port}!`);
-});
+app.listen(8080);
