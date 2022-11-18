4ba8665ce7c3bb798cc2ad05a4b6379224045067
Guillaume Bertello <guillaume.bertello@gmail.com>
2022-11-18 21:56:59 +0100

    Add working test

--- app/test.py
+++ app/test.py
@@ -4,7 +4,7 @@ import subprocess
 
 def test_create():
   r = requests.get("http://localhost:3000/")
-  assert r.text == "Hello world 2!"
+  assert r.text == "Hello world 3!"
 
 if __name__ == "__main__":
   subprocess.run(["python3", "-m", "pytest", __file__])
